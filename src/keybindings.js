78d41d2a405bf749a016a30eeaa6c03e703ac1a2
Manuel Vögele <develop@manuel-voegele.de>
2022-01-05 21:12:52 +0100

    Don't suppress other key events in the toggleSecretDoors keybinding (fixes #12)

--- src/keybindings.js
+++ src/keybindings.js
@@ -6,10 +6,15 @@ export function registerKeybindings() {
 	game.keybindings.register(settingsKey, "toggleSecretDoor", {
 		name: "smart-doors.keybindings.toggleSecretDoor.name",
 		hint: "smart-doors.keybindings.toggleSecretDoor.hint",
-		onDown: () => toggleSecretDoor = true,
-		onUp: () => toggleSecretDoor = false,
+		onDown: handleToggleSecretDoor,
+		onUp: handleToggleSecretDoor,
 		restricted: true,
 		editable: [{key: "AltLeft"}],
 		precedence: -1,
 	});
+}
+
+function handleToggleSecretDoor(event) {
+	toggleSecretDoor = !event.up;
+	return false;
 }
